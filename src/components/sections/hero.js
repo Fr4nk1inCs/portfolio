3c9a3aa83db473c3a8de7245da2497089f49e94b
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-08-06 03:29:04 +0800

    Change text info to my own ones

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -3,6 +3,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 import { navDelay, loaderDelay } from '@utils';
 import { usePrefersReducedMotion } from '@hooks';
+import { email } from '@config';
 
 const StyledHeroSection = styled.section`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -60,28 +61,26 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Shen Fu.</h2>;
+  const three = <h3 className="big-heading">I’m ready for any coding.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a senior undergraduate student at{' '}
+        <a href="https://en.scgy.ustc.edu.cn/" target="_blank" rel="noreferrer">
+          USTC-SCGY
         </a>
-        .
+        , majoring in CS. I’m also a daily Linux user who love{' '}
+        <a href="https://www.reddit.com/r/unixporn/" target="_blank" rel="noreferrer">
+          Linux Ricing
+        </a>
+        . Currently, I’m studying how to provide systematic support for AI/ML and speed them up.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href={`mailto:${email}`} target="_blank" rel="noreferrer">
+      Get in touch!
     </a>
   );
 
--- src/config.js
+++ src/config.js
@@ -1,26 +1,14 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'sh.fu@outlook.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://github.com/Fr4nk1in-USTC',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
-    {
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://twitter.com/Fr4nk1inCs',
     },
   ],
 
@@ -30,9 +18,13 @@ module.exports = {
       url: '/#about',
     },
     {
-      name: 'Experience',
-      url: '/#jobs',
+      name: 'Education',
+      url: '/#edu',
     },
+    // {
+    //   name: 'Experience',
+    //   url: '/#jobs',
+    // },
     {
       name: 'Work',
       url: '/#projects',
