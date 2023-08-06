c56ba813c0a65aaa83e0d4c4724f5c9d3d12ae42
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-08-06 15:56:30 +0800

    Update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Shen Fu',
+        short_name: 'Shen Fu',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
